29bb01d3cd298dd45e586c6617cab8381ef4d5cd
etzi71 <73713771+etzi71@users.noreply.github.com>
2021-06-20 21:18:08 +0300

    Update update_build_number.sh

--- update_build_number.sh
+++ update_build_number.sh
@@ -1,1 +1,1 @@
-sed -i s/"tag: \"[0-9][0-9]\""/\"$1\"/g /home/etzi71/workspace/build_images/helm_chart/values.yaml
+sed -i s/"tag: \"[0-9][0-9]\""/"tag: \"$1\""/g /home/etzi71/workspace/build_images/helm_chart/values.yaml
